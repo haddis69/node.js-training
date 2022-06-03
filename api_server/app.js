a7543b1278cfe8cc8d1387c6850e71361a6ffdba
haddis69 <1474154683@qq.com>
2022-06-03 21:49:14 +0800

    项目初始化

---
+++ api_server/app.js
@@ -0,0 +1,21 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+
+// 导入 cors 中间件
+const cors = require('cors')
+// 将 cors 注册为全局中间件
+app.use(cors())
+
+//通过如下的代码，配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
+app.use(express.urlencoded({ extended: false }))
+
+// 导入并注册用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+  console.log('api server running at http://127.0.0.1:3007')
+})

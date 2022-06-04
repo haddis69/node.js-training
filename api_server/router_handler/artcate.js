a75f0804c309248aa07a59365ff0af1900f79478
haddis69 <1474154683@qq.com>
2022-06-04 22:18:49 +0800

    根据id更新文章分类

--- api_server/router_handler/artcate.js
+++ api_server/router_handler/artcate.js
@@ -59,3 +59,51 @@ exports.deleteCateById = (req, res) => {
         res.cc('删除文章分类成功！', 0)
     })
 }
+
+// 根据 Id 获取文章分类的处理函数
+exports.getArtCateById = (req, res) => {
+    const sql = `select * from ev_article_cate where id=?`
+    db.query(sql, req.params.id, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // SQL 语句执行成功，但是没有查询到任何数据
+        if (results.length !== 1) return res.cc('获取文章分类数据失败！')
+
+        // 把数据响应给客户端
+        res.send({
+            status: 0,
+            message: '获取文章分类数据成功！',
+            data: results[0],
+        })
+    })
+}
+// 更新文章分类的处理函数
+exports.updateCateById = (req, res) => {
+    // 定义查询 分类名称 与 分类别名 是否被占用的 SQL 语句
+    const sql = `select * from ev_article_cate where Id<>? and (name=? or alias=?)`
+    // 执行查重操作
+    db.query(sql, [req.body.Id, req.body.name, req.body.alias], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // 分类名称 和 分类别名 都被占用
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // 分类名称 或 分类别名 被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+
+        const sql = `update ev_article_cate set ? where Id=?`
+        db.query(sql, [req.body, req.body.Id], (err, results) => {
+            // 执行 SQL 语句失败
+            if (err) return res.cc(err)
+          
+            // SQL 语句执行成功，但是影响行数不等于 1
+            if (results.affectedRows !== 1) return res.cc('更新文章分类失败！')
+          
+            // 更新文章分类成功
+            res.cc('更新文章分类成功！', 0)
+          })
+    })
+}

e3f6e84e1f0849afb3d273787700198b7f1b29ce
haddis69 <1474154683@qq.com>
2022-06-04 21:59:18 +0800

    根据id删除文章

--- api_server/router_handler/artcate.js
+++ api_server/router_handler/artcate.js
@@ -35,12 +35,27 @@ exports.addArticleCates = (req, res) => {
         db.query(sql, req.body, (err, results) => {
             // SQL 语句执行失败
             if (err) return res.cc(err)
-          
+
             // SQL 语句执行成功，但是影响行数不等于 1
             if (results.affectedRows !== 1) return res.cc('新增文章分类失败！')
-          
+
             // 新增文章分类成功
             res.cc('新增文章分类成功！', 0)
         })
+    })
+}
+
+// 删除文章分类的处理函数
+exports.deleteCateById = (req, res) => {
+    const sql = `update ev_article_cate set is_delete=1 where id=?`
+    db.query(sql, req.params.id, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // SQL 语句执行成功，但是影响行数不等于 1
+        if (results.affectedRows !== 1) return res.cc('删除文章分类失败！')
+
+        // 删除文章分类成功
+        res.cc('删除文章分类成功！', 0)
     })
 }
--- api_server/schema/artcate.js
+++ api_server/schema/artcate.js
@@ -4,11 +4,18 @@ const joi = require('joi')
 // 定义 分类名称 和 分类别名 的校验规则
 const name = joi.string().required()
 const alias = joi.string().alphanum().required()
+const id = joi.number().integer().min(1).required()
 
 // 校验规则对象 - 添加分类
 exports.add_cate_schema = {
-  body: {
-    name,
-    alias,
-  },
+    body: {
+        name,
+        alias,
+    },
+}
+// 校验规则对象 - 删除分类
+exports.delete_cate_schema = {
+    params: {
+        id,
+    },
 }

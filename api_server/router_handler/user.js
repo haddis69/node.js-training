76b060c428d347692da9a37b4b50b1cc02dacc22
haddis69 <1474154683@qq.com>
2022-06-04 10:01:27 +0800

    优化代码

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -6,18 +6,18 @@ exports.regUser = (req, res) => {
     const userinfo = req.body;
     // 判断数据是否合法
     if (!userinfo.username || !userinfo.password) {
-        return res.send({ status: 1, message: '用户名或密码不能为空！' })
+        return res.cc('用户名或密码不能为空！');
     }
     //定义 SQL 语句
     const sql = `select * from ev_users where username=?`;
     db.query(sql, [userinfo.username], function (err, results) {
         // 执行 SQL 语句失败
         if (err) {
-            return res.send({ status: 1, message: err.message })
+            return res.cc(err);
         }
         // 用户名被占用
         if (results.length > 0) {
-            return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+            return res.cc('用户名被占用，请更换其他用户名！');
         }
         // 对用户的密码,进行 bcrype 加密，返回值是加密之后的密码字符串
         userinfo.password = bcrypt.hashSync(userinfo.password, 10)
@@ -25,13 +25,13 @@ exports.regUser = (req, res) => {
         const sql = 'insert into ev_users set ?'
         db.query(sql, { username: userinfo.username, password: userinfo.password }, function (err, results) {
             // 执行 SQL 语句失败
-            if (err) return res.send({ status: 1, message: err.message })
+            if (err) return res.cc(err);
             // SQL 语句执行成功，但影响行数不为 1
             if (results.affectedRows !== 1) {
-              return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+              return res.cc('注册用户失败，请稍后再试！')
             }
             // 注册成功
-            res.send({ status: 0, message: '注册成功！' })
+            res.cc('注册成功！',0)
         })
     })
 }

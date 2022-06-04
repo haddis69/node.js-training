ffce605277d2f214d98842f62474f8f43ccf78ea
haddis69 <1474154683@qq.com>
2022-06-04 14:58:55 +0800

    更换头像

--- api_server/router_handler/userinfo.js
+++ api_server/router_handler/userinfo.js
@@ -71,3 +71,17 @@ exports.updatePassword = (req, res) => {
         })
     })
 }
+// 更新用户头像的处理函数
+exports.updateAvatar = (req, res) => {
+    const sql = 'update ev_users set user_pic=? where id=?'
+    db.query(sql, [req.body.avatar, req.user.id], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+      
+        // 执行 SQL 语句成功，但是影响行数不等于 1
+        if (results.affectedRows !== 1) return res.cc('更新头像失败！')
+      
+        // 更新用户头像成功
+        return res.cc('更新头像成功！', 0)
+    })
+}

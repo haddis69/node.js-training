15d287265eb223185a686f1660a97b32568b90a0
haddis69 <1474154683@qq.com>
2022-06-04 14:46:41 +0800

    更新密码

--- api_server/router/userinfo.js
+++ api_server/router/userinfo.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const expressJoi = require('@escook/express-joi');
-const { update_userinfo_schema } = require('../schema/user')
+const { update_userinfo_schema, update_password_schema } = require('../schema/user')
 // 导入用户信息的处理函数模块
 const userinfo_handler = require('../router_handler/userinfo')
 
@@ -9,5 +9,7 @@ const userinfo_handler = require('../router_handler/userinfo')
 router.get('/userinfo', userinfo_handler.getUserInfo)
 // 更新用户的基本信息
 router.post('/userinfo', expressJoi(update_userinfo_schema),userinfo_handler.updateUserInfo)
+// 重置密码的路由
+router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 
 module.exports = router
